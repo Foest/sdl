80dc2fe2211685e42d5d2d91071b5662b946f5cb
foest <foest88@gmail.com>
2013-03-28 15:13:23 -0700

    add load_image, apply_surface functions

--- hello_world_sdl.cpp
+++ hello_world_sdl.cpp
@@ -1,4 +1,47 @@
 #include "SDL/SDL.h"
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+SDL_Surface *load_image(std::string filename)
+{
+  //Temporary storage for the image that's loaded
+  SDL_Surface *loadedImage = NULL;
+
+  //Optimized image that will be used
+  SDL_Surface *optimizedImage = NULL;
+
+  //Load the image
+  loadedImage = SDL_LoadBMP(filename.c_str());
+
+  //If nothing went wrong in loading the image
+  if(loadedImage != NULL)
+  {
+    //Create an optimized image
+    optimizedImage = SDL_DisplayFormat(loadedImage);
+
+    //Free the old image
+    SDL_FreeSurface(loadedImage);
+  }
+
+  //Return the optimized image
+  return optimizedImage;
+}
+
+void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination)
+{
+  //Make a temporary rectangle to hold the offsets
+  SDL_Rect offset;
+
+  //Give the offsets to the rectangle
+  offset.x = x;
+  offset.y = y;
+
+  //Blit the surface
+  SDL_BlitSurface(source, NULL, destination, &offset);
+}
 
 int main(int argc, char* args[])
 {
