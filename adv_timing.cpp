f0c1f894517543096c6f38ea33f47d2c7c1428c3
foest <foest88@gmail.com>
2013-04-11 15:09:52 -0700

    add adv_timing.cpp

---
+++ adv_timing.cpp
@@ -0,0 +1,203 @@
+#include "iostream"
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include <sstream>
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+SDL_Surface *background = NULL;
+SDL_Surface *screen = NULL;
+SDL_Surface *seconds = NULL;
+SDL_Surface *startStop= NULL;
+
+SDL_Event event;
+TTF_Font *font = NULL;
+SDL_Color textColor = {0, 0, 0};
+
+void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination, SDL_Rect *clip = NULL)
+{
+  //Make a temporary rectangle to hold the offsets
+  SDL_Rect offset;
+
+  //Give the offsets to the rectangle
+  offset.x = x;
+  offset.y = y;
+
+  //Blit the surface
+  SDL_BlitSurface(source, clip, destination, &offset);
+}
+
+SDL_Surface *load_image(std::string filename)
+{
+  //Temporary storage for the image that's loaded
+  SDL_Surface *loadedImage = NULL;
+
+  //Optimized image that will be used
+  SDL_Surface *optimizedImage = NULL;
+
+  //Load the image
+  loadedImage = IMG_Load(filename.c_str());
+
+  //If nothing went wrong in loading the image
+  if(loadedImage != NULL)
+  {
+    //Create an optimized image
+    optimizedImage = SDL_DisplayFormat(loadedImage);
+
+    //Free the old image
+    SDL_FreeSurface(loadedImage);
+  }
+
+  //If the image was optimized without error
+  if(optimizedImage != NULL)
+  {
+    //Map the color key
+    Uint32 colorkey = SDL_MapRGB(optimizedImage->format, 0, 0xFF, 0xFF);
+
+    //Set all pixels of color R 0, G 0xFF, B 0xFF to be transparent
+    SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY, colorkey);
+  }
+
+
+  //Return the optimized image
+  return optimizedImage;
+}
+
+bool init()
+{
+  //Init SDL subsystems
+  if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+  {
+    return false;
+  }
+
+  //Set up screen
+  screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+
+  //If there was an error setting up the screen
+  if(screen == NULL)
+  {
+    return false;
+  }
+
+  if(TTF_Init() == -1)
+  {
+    return false;
+  }
+
+  SDL_WM_SetCaption("TIMING!!", NULL);
+
+  return true;
+}
+
+bool load_files()
+{
+  //Load image
+  background = load_image("background.png");
+
+  //Open the font
+  font = TTF_OpenFont("lazy.ttf", 30);
+
+  //If there was an error loading the images
+  if(background == NULL)
+  {
+    return false;
+  }
+
+  if(font == NULL)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+void clean_up()
+{
+  //Free the images
+  SDL_FreeSurface(background);
+
+  TTF_CloseFont(font);
+
+  TTF_Quit();
+
+  //Quit SDL
+  SDL_Quit();
+}
+
+
+
+int main(int argc, char* args[])
+{
+  bool quit = false;
+  Uint32 start = 0;
+  bool running = true;
+
+  if(init() == false)
+  {
+    return 1;
+  }
+
+  //Load the files
+  if(load_files() == false)
+  {
+    return 1;
+  }
+
+  startStop = TTF_RenderText_Solid(font, "Press S to start or stop the timer", textColor);
+  start = SDL_GetTicks();
+
+
+  //While user hasn't quit
+  while(quit == false)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_KEYDOWN)
+      {
+        if(event.key.keysym.sym == SDLK_s)
+        {
+          if(running == true)
+          {
+            running = false;
+            start = 0;
+          }
+          else
+          {
+            running = true;
+            start = SDL_GetTicks();
+          }
+        }
+      }
+      else if(event.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+    }
+      apply_surface(0, 0, background, screen);
+      apply_surface((SCREEN_WIDTH - startStop->w) / 2, 200, startStop, screen);
+      if(running == true)
+      {
+        std::stringstream time;
+        time << "Timer: " << SDL_GetTicks() - start;
+
+        seconds = TTF_RenderText_Solid(font, time.str().c_str(), textColor);
+
+        apply_surface((SCREEN_WIDTH - seconds->w) / 2, 50, seconds, screen);
+
+        SDL_FreeSurface(seconds);
+      }
+
+      if(SDL_Flip(screen) == -1)
+      {
+        return 1;
+      }
+  }
+
+  clean_up();
+  return 0;
+}
