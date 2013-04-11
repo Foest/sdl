d14f5ce92f043b3dd5cffe17afc1ee44c9041706
foest <foest88@gmail.com>
2013-04-11 14:41:48 -0700

    update timing.cpp

--- timing.cpp
+++ timing.cpp
@@ -2,7 +2,7 @@
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include "SDL/SDL_ttf.h"
-#include "SDL/SDL_mixer.h"
+#include <sstream>
 #include <string>
 
 const int SCREEN_WIDTH = 640;
@@ -11,18 +11,12 @@ const int SCREEN_BPP = 32;
 
 SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
-SDL_Surface *message = NULL;
+SDL_Surface *seconds = NULL;
+SDL_Surface *startStop= NULL;
 
 SDL_Event event;
 TTF_Font *font = NULL;
 SDL_Color textColor = {0, 0, 0};
-
-Mix_Music *music = NULL;
-
-Mix_Chunk *scratch = NULL;
-Mix_Chunk *high= NULL;
-Mix_Chunk *med= NULL;
-Mix_Chunk *low= NULL;
 
 void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination, SDL_Rect *clip = NULL)
 {
@@ -95,12 +89,7 @@ bool init()
     return false;
   }
 
-  if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
-  {
-    return false;
-  }
-
-  SDL_WM_SetCaption("SOUNDS!!", NULL);
+  SDL_WM_SetCaption("TIMING!!", NULL);
 
   return true;
 }
@@ -124,23 +113,6 @@ bool load_files()
     return false;
   }
 
-  music = Mix_LoadMUS("beat.wav");
-
-  if(music == NULL)
-  {
-    return false;
-  }
-
-  scratch = Mix_LoadWAV("scratch.wav");
-  high = Mix_LoadWAV("high.wav");
-  med = Mix_LoadWAV("medium.wav");
-  low = Mix_LoadWAV("low.wav");
-
-  if((scratch == NULL) || (high == NULL) || (med == NULL) || (low == NULL))
-  {
-    return false;
-  }
-
   return true;
 }
 
@@ -148,15 +120,6 @@ void clean_up()
 {
   //Free the images
   SDL_FreeSurface(background);
-
-  Mix_FreeChunk(scratch);
-  Mix_FreeChunk(high);
-  Mix_FreeChunk(med);
-  Mix_FreeChunk(low);
-
-  Mix_FreeMusic(music);
-
-  Mix_CloseAudio();
 
   TTF_CloseFont(font);
 
@@ -171,6 +134,8 @@ void clean_up()
 int main(int argc, char* args[])
 {
   bool quit = false;
+  Uint32 start = 0;
+  bool running = true;
 
   if(init() == false)
   {
@@ -180,45 +145,12 @@ int main(int argc, char* args[])
   //Load the files
   if(load_files() == false)
   {
-    std::cout << "1";
     return 1;
   }
 
-  apply_surface(0, 0, background, screen);
-  message = TTF_RenderText_Solid(font, "1, 2, 3, or 4 to play a sound effect", textColor);
+  startStop = TTF_RenderText_Solid(font, "Press S to start or stop the timer", textColor);
+  start = SDL_GetTicks();
 
-  if(message == NULL)
-  {
-    return 1;
-  }
-
-  apply_surface((SCREEN_WIDTH - message->w) / 2, 100, message, screen);
-
-  SDL_FreeSurface(message);
-
-  message = TTF_RenderText_Solid(font, "Press 9 to play or pause the music", textColor);
-  if(message == NULL)
-  {
-    return 1;
-  }
-
-  apply_surface((SCREEN_WIDTH - message->w) / 2, 200, message, screen);
-  SDL_FreeSurface(message);
-  message = TTF_RenderText_Solid(font, "Press 0 to stop the music", textColor);
-
-  if(message == NULL)
-  {
-    return 1;
-  }
-
-  apply_surface((SCREEN_WIDTH - message->w) / 2, 300, message, screen);
-  SDL_FreeSurface(message);
-
-
-  if(SDL_Flip(screen) == -1)
-    {
-      return 1;
-    }
 
   //While user hasn't quit
   while(quit == false)
@@ -227,73 +159,43 @@ int main(int argc, char* args[])
     {
       if(event.type == SDL_KEYDOWN)
       {
-        if(event.key.keysym.sym == SDLK_1)
+        if(event.key.keysym.sym == SDLK_s)
         {
-          if(Mix_PlayChannel(-1, scratch, 0) == -1)
+          if(running == true)
           {
-            return 1;
+            running = false;
+            start = 0;
           }
-        }
-
-        else if(event.key.keysym.sym == SDLK_2)
-        {
-          if(Mix_PlayChannel(-1, high, 0) == -1)
-          {
-            return 1;
-          }
-        }
-
-        else if(event.key.keysym.sym == SDLK_3)
-        {
-          if(Mix_PlayChannel(-1, med, 0) == -1)
-          {
-            return 1;
-          }
-        }
-
-        else if(event.key.keysym.sym== SDLK_4)
-        {
-          if(Mix_PlayChannel(-1, low, 0) == -1)
-          {
-            return 1;
-          }
-        }
-
-        else if(event.key.keysym.sym == SDLK_9)
-        {
-          if(Mix_PlayingMusic() == 0)
-          {
-            if(Mix_PlayMusic(music, -1) == -1)
-            {
-              return 1;
-            }
-          }
-
-
-        else
-        {
-          if(Mix_PausedMusic() == 1)
-          {
-            Mix_ResumeMusic();
-          }
-
           else
           {
-            Mix_PauseMusic();
+            running = true;
+            start = SDL_GetTicks();
           }
         }
       }
-      else if (event.key.keysym.sym == SDLK_0)
-          {
-            Mix_HaltMusic();
-          }
-      }
-
-      if(event.type == SDL_QUIT)
+      else if(event.type == SDL_QUIT)
       {
         quit = true;
       }
     }
+      apply_surface(0, 0, background, screen);
+      apply_surface((SCREEN_WIDTH - startStop->w) / 2, 200, startStop, screen);
+      if(running == true)
+      {
+        std::stringstream time;
+        time << "Timer: " << SDL_GetTicks() - start;
+
+        seconds = TTF_RenderText_Solid(font, time.str().c_str(), textColor);
+
+        apply_surface((SCREEN_WIDTH - seconds->w) / 2, 50, seconds, screen);
+
+        SDL_FreeSurface(seconds);
+      }
+
+      if(SDL_Flip(screen) == -1)
+      {
+        return 1;
+      }
   }
 
   clean_up();
