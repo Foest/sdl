11ffba5c28a214866f290c6716b18e13a291fadf
foest <foest88@gmail.com>
2013-04-14 15:37:30 -0700

    add Timer class, methods

--- adv_timing.cpp
+++ adv_timing.cpp
@@ -18,6 +18,92 @@ SDL_Event event;
 TTF_Font *font = NULL;
 SDL_Color textColor = {0, 0, 0};
 
+class Timer
+{
+  private:
+    int startTicks;
+    int pausedTicks;
+
+    bool paused;
+    bool started;
+
+  public:
+    Timer();
+    void start();
+    void stop();
+    void pause();
+    void unpause();
+    int get_ticks();
+    bool is_started();
+    bool is_paused();
+};
+
+Timer::Timer()
+{
+  startTicks = 0;
+  pausedTicks = 0;
+  paused = false;
+  started = false;
+}
+
+void Timer::start()
+{
+  started = true;
+  paused = false;
+  startTicks = SDL_GetTicks();
+}
+
+void Timer::stop()
+{
+  started = false;
+  paused = false;
+}
+
+int Timer::get_ticks()
+{
+  if(started == true)
+  {
+    if(paused == true)
+    {
+      return pausedTicks;
+    }
+    else
+    {
+      return SDL_GetTicks() - startTicks;
+    }
+  }
+  return 0;
+}
+
+void Timer::pause()
+{
+  if((started == true) && (paused == false))
+    {
+      paused = true;
+      pausedTicks = SDL_GetTicks() - startTicks;
+    }
+}
+
+void Timer::unpause()
+{
+  if(paused == true)
+  {
+    paused = false;
+    startTicks = SDL_GetTicks() - pausedTicks;
+    pausedTicks = 0;
+  }
+}
+
+bool Timer::is_started()
+{
+  return started;
+}
+
+bool Timer::is_paused()
+{
+  return paused;
+}
+
 void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination, SDL_Rect *clip = NULL)
 {
   //Make a temporary rectangle to hold the offsets
