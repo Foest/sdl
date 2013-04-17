d7792ca4abbd13bc75744cfc310aafca7f5e65f5
foest <foest88@gmail.com>
2013-04-17 16:00:38 -0700

    add Dot class

--- motion.cpp
+++ motion.cpp
@@ -17,6 +17,66 @@ SDL_Event event;
 TTF_Font *font = NULL;
 SDL_Color textColor = {0, 0, 0};
 
+class Dot
+{
+  private:
+    int x, y;
+    int xVel, yVel;
+  public:
+    Dot();
+    void handle_input();
+    void move();
+    void show();
+};
+
+Dot::Dot()
+{
+  x = 0;
+  y = 0;
+
+  xVel = 0;
+  yVel = 0;
+}
+
+void Dot::handle_input()
+{
+  if(event.type == SDL_KEYDOWN)
+  {
+    switch(event.key.keysym.sym)
+    {
+      case SDLK_UP: yVel -= DOT_HEIGHT / 2; break;
+      case SDLK_DOWN: yVel += DOT_HEIGHT / 2; break;
+      case SDLK_RIGHT: xVel += DOT_WIDTH / 2; break;
+      case SDLK_LEFT: xVel -= DOT_WIDTH / 2; break;
+    }
+  }
+  else if(event.type == SDL_KEYUP)
+  {
+    switch(event.key.keysym.sym)
+    {
+      case SDLK_UP: yVel += DOT_HEIGHT / 2; break;
+      case SDLK_DOWN: yVel -= DOT_HEIGHT / 2; break;
+      case SDLK_RIGHT: xVel -= DOT_WIDTH / 2; break;
+      case SDLK_LEFT: xVel += DOT_WIDTH / 2; break;
+    }
+  }
+}
+
+void Dot::move()
+{
+  x += xVel;
+  if((x < 0) || (x + DOT_WIDTH > SCREEN_WIDTH))
+  {
+    x -=xVel;
+  }
+
+  y += yVel;
+  if((y < 0) || (y + DOT_HEIGHT > SCREEN_HEIGHT))
+  {
+    y -= yVel;
+  }
+}
+
 class Timer
 {
   private:
