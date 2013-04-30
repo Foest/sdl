c4355bd7422979b4c936ea4e811f4695739c7b21
foest <foest88@gmail.com>
2013-04-29 21:07:43 -0700

    add check_collision()

--- circular_collision_detection.cpp
+++ circular_collision_detection.cpp
@@ -136,6 +136,13 @@ bool check_collision(Circle &A, Circle &B)
   return false;
 }
 
+double distance(int x1, int y1, int x2, int y2)
+{
+  return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+}
+
+
+
 struct Circle
 {
   int x, y;
