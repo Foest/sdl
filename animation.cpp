dc6913526117d5f8daca1451179fdb789ff24879
foest <foest88@gmail.com>
2013-05-06 19:41:15 -0700

    add Foo methods

--- animation.cpp
+++ animation.cpp
@@ -466,37 +466,72 @@ void Dot::show()
 //  apply_surface(c.x - c.r, c.y - c.r, dot, screen);
 }
 
-void set_clips() {
-clipsRight[ 0 ].x = 0;
-clipsRight[ 0 ].y = 0;
-clipsRight[ 0 ].w = FOO_WIDTH;
-clipsRight[ 0 ].h = FOO_HEIGHT;
-clipsRight[ 1 ].x = FOO_WIDTH;
-clipsRight[ 1 ].y = 0;
-clipsRight[ 1 ].w = FOO_WIDTH;
-clipsRight[ 1 ].h = FOO_HEIGHT;
-clipsRight[ 2 ].x = FOO_WIDTH * 2;
-clipsRight[ 2 ].y = 0;
-clipsRight[ 2 ].w = FOO_WIDTH;
-clipsRight[ 2 ].h = FOO_HEIGHT;
-clipsRight[ 3 ].x = FOO_WIDTH * 3;
-clipsRight[ 3 ].y = 0;
-clipsRight[ 3 ].w = FOO_WIDTH;
-clipsRight[ 3 ].h = FOO_HEIGHT;
-clipsLeft[ 0 ].x = 0;
-clipsLeft[ 0 ].y = FOO_HEIGHT;
-clipsLeft[ 0 ].w = FOO_WIDTH;
-clipsLeft[ 0 ].h = FOO_HEIGHT;
-clipsLeft[ 1 ].x = FOO_WIDTH;
-clipsLeft[ 1 ].y = FOO_HEIGHT;
-clipsLeft[ 1 ].w = FOO_WIDTH;
-clipsLeft[ 1 ].h = FOO_HEIGHT;
-clipsLeft[ 2 ].x = FOO_WIDTH * 2;
-clipsLeft[ 2 ].y = FOO_HEIGHT;
-clipsLeft[ 2 ].w = FOO_WIDTH;
-clipsLeft[ 2 ].h = FOO_HEIGHT;
-clipsLeft[ 3 ].x = FOO_WIDTH * 3;
-clipsLeft[ 3 ].y = FOO_HEIGHT;
-clipsLeft[ 3 ].w = FOO_WIDTH;
-clipsLeft[ 3 ].h = FOO_HEIGHT;
+Foo::Foo()
+{
+  offSet = 0;
+  velocity = 0;
+  frame = 0;
+  status = FOO_RIGHT;
+}
+
+void Foo::handle_events()
+{
+  if(event.type == SDL_KEYDOWN)
+  {
+    switch(event.key.keysym.sym)
+    {
+      case SDLK_RIGHT: velocity += FOO_WIDTH / 4; break;
+      case SDLK_LEFT: velocity -= FOO_WIDTH / 4; break;
+    }
+  }
+  else if(event.type = SDL_KEYUP)
+  {
+    switch(event.key.keysym.sym)
+    {
+      case SDLK_RIGHT: velocity -= FOO_WIDTH / 4; break;
+      case SDLK_LEFT: velocity += FOO_WIDTH / 4; break;
+    }
+  }
+}
+
+void Foo::move()
+{
+  offSet += velocity;
+
+  if((offSet < 0) || (offSet + FOO_WIDTH > SCREEN_WIDTH))
+  {
+    offSet -= velocity;
+  }
+}
+
+void Foo::show()
+{
+  if(velocity < 0)
+  {
+    status = FOO_LEFT;
+    frame++;
+  }
+  else if(velocity > 0)
+  {
+    status = FOO_RIGHT;
+    frame++;
+  }
+  else
+  {
+    frame = 0;
+  }
+
+  if(frame >= 4)
+  {
+    frame = 0;
+  }
+
+  if(status == FOO_RIGHT)
+  {
+    apply_surface(offSet, SCREEN_HEIGHT - FOO_HEIGHT, foo, screen, &clipsRight[frame]);
+  }
+  else if(status = FOO_LEFT)
+  {
+    apply_surface(offSet, SCREEN_HEIGHT - FOO_HEIGHT, foo, screen, &clipsLeft[frame]);
+  }
 }
