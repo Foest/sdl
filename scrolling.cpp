ba02b3abc1c762a64419f77a0b5277c50dcfcdf7
foest <foest88@gmail.com>
2013-05-18 21:42:21 -0700

    implement Dot::set_camera()

--- scrolling.cpp
+++ scrolling.cpp
@@ -429,3 +429,27 @@ void Dot::move()
     y -= yVel;
   }
 }
+
+void Dot::set_camera()
+{
+  camera.x = (x + DOT_WIDTH / 2) - SCREEN_WIDTH / 2;
+  camera.y = (y + DOT_HEIGHT / 2) - SCREEN_HEIGHT / 2;
+
+  if(camera.x < 0)
+  {
+    camera.x = 0;
+  }
+  if(camera.y < 0)
+  {
+    camera.y = 0;
+  }
+  if(camera.x > LEVEL_WIDTH - camera.w)
+  {
+    camera.x = LEVEL_WIDTH - camera.w;
+  }
+  if(camera.y > LEVEL_HEIGHT - camera.h)
+  {
+    camera.y = LEVEL_HEIGHT - camera.h;
+  }
+}
+
