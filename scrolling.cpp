3ee8786ed0eb52e085ecc80b58c98927904be49c
foest <foest88@gmail.com>
2013-05-30 19:05:21 -0700

    remove set_clips(), Foo

--- scrolling.cpp
+++ scrolling.cpp
@@ -12,10 +12,6 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 const int FRAMES_PER_SECOND = 20;
-const int FOO_WIDTH = 64;
-const int FOO_HEIGHT = 205;
-const int FOO_RIGHT = 0;
-const int FOO_LEFT = 1;
 const int LEVEL_WIDTH = 1280;
 const int LEVEL_HEIGHT = 960;
 const int DOT_WIDTH = 20;
@@ -24,11 +20,8 @@ const int DOT_HEIGHT = 20;
 //Globals
 SDL_Surface *dot = NULL;
 SDL_Surface *background = NULL;
-SDL_Surface *foo = NULL;
 SDL_Surface *screen = NULL;
 SDL_Event event;
-SDL_Rect clipsLeft[4];
-SDL_Rect clipsRight[4];
 SDL_Rect camera = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 
 //Prototypes
@@ -38,28 +31,12 @@ void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination, 
 SDL_Surface *load_image(std::string filename);
 bool load_files();
 void clean_up();
-void set_clips();
 
 //Structs/Classes
 struct Circle
 {
   int x, y;
   int r;
-};
-
-class Foo
-{
-  private:
-    int offSet;
-    int velocity;
-    int frame;
-    int status;
-
-  public:
-    Foo();
-    void handle_events();
-    void move();
-    void show();
 };
 
 class Dot
@@ -110,7 +87,6 @@ int main(int argc, char* args[])
     return 1;
   }
 
-  set_clips();
   Timer fps;
   Dot myDot;
 
@@ -250,42 +226,6 @@ void clean_up()
   SDL_Quit();
 }
 
-void set_clips()
-{
-  clipsRight[ 0 ].x = 0;
-  clipsRight[ 0 ].y = 0;
-  clipsRight[ 0 ].w = FOO_WIDTH;
-  clipsRight[ 0 ].h = FOO_HEIGHT;
-  clipsRight[ 1 ].x = FOO_WIDTH;
-  clipsRight[ 1 ].y = 0;
-  clipsRight[ 1 ].w = FOO_WIDTH;
-  clipsRight[ 1 ].h = FOO_HEIGHT;
-  clipsRight[ 2 ].x = FOO_WIDTH * 2;
-  clipsRight[ 2 ].y = 0;
-  clipsRight[ 2 ].w = FOO_WIDTH;
-  clipsRight[ 2 ].h = FOO_HEIGHT;
-  clipsRight[ 3 ].x = FOO_WIDTH * 3;
-  clipsRight[ 3 ].y = 0;
-  clipsRight[ 3 ].w = FOO_WIDTH;
-  clipsRight[ 3 ].h = FOO_HEIGHT;
-  clipsLeft[ 0 ].x = 0;
-  clipsLeft[ 0 ].y = FOO_HEIGHT;
-  clipsLeft[ 0 ].w = FOO_WIDTH;
-  clipsLeft[ 0 ].h = FOO_HEIGHT;
-  clipsLeft[ 1 ].x = FOO_WIDTH;
-  clipsLeft[ 1 ].y = FOO_HEIGHT;
-  clipsLeft[ 1 ].w = FOO_WIDTH;
-  clipsLeft[ 1 ].h = FOO_HEIGHT;
-  clipsLeft[ 2 ].x = FOO_WIDTH * 2;
-  clipsLeft[ 2 ].y = FOO_HEIGHT;
-  clipsLeft[ 2 ].w = FOO_WIDTH;
-  clipsLeft[ 2 ].h = FOO_HEIGHT;
-  clipsLeft[ 3 ].x = FOO_WIDTH * 3;
-  clipsLeft[ 3 ].y = FOO_HEIGHT;
-  clipsLeft[ 3 ].w = FOO_WIDTH;
-  clipsLeft[ 3 ].h = FOO_HEIGHT;
-}
-
 Timer::Timer()
 {
   startTicks = 0;
@@ -352,14 +292,6 @@ bool Timer::is_paused()
   return paused;
 }
 
-Foo::Foo()
-{
-  offSet = 0;
-  velocity = 0;
-  frame = 0;
-  status = FOO_RIGHT;
-}
-
 void Dot::handle_input()
 {
   if(event.type == SDL_KEYDOWN)
@@ -381,48 +313,6 @@ void Dot::handle_input()
       case SDLK_UP: yVel += DOT_HEIGHT / 2; break;
       case SDLK_DOWN: yVel -= DOT_HEIGHT / 2; break;
     }
-  }
-}
-
-void Foo::move()
-{
-  offSet += velocity;
-
-  if((offSet < 0) || (offSet + FOO_WIDTH > SCREEN_WIDTH))
-  {
-    offSet -= velocity;
-  }
-}
-
-void Foo::show()
-{
-  if(velocity < 0)
-  {
-    status = FOO_LEFT;
-    frame++;
-  }
-  else if(velocity > 0)
-  {
-    status = FOO_RIGHT;
-    frame++;
-  }
-  else
-  {
-    frame = 0;
-  }
-
-  if(frame >= 4)
-  {
-    frame = 0;
-  }
-
-  if(status == FOO_RIGHT)
-  {
-    apply_surface(offSet, SCREEN_HEIGHT - FOO_HEIGHT, foo, screen, &clipsRight[frame]);
-  }
-  else if(status = FOO_LEFT)
-  {
-    apply_surface(offSet, SCREEN_HEIGHT - FOO_HEIGHT, foo, screen, &clipsLeft[frame]);
   }
 }
 
@@ -478,9 +368,3 @@ void Dot::show()
 {
   apply_surface(x - camera.x, y - camera.y, dot, screen);
 }
-
-//TODO: implement handle_input()
-void Dot::handle_input()
-{
-}
-
