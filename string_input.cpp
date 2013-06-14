b36d40a5bb6c5d27400fbd9dc834f48f27bd80f4
foest <foest88@gmail.com>
2013-06-13 23:04:12 -0700

    add handle_input() implementation

--- string_input.cpp
+++ string_input.cpp
@@ -390,5 +390,36 @@ void StringInput::handle_input()
   if(event.type == SDL_KEYDOWN)
   {
     std::string temp = str;
+
+    if(str.length() <= 16)
+    {
+      if(event.key.keysym.unicode == (Uint16)' ')
+      {
+        str += (char)event.key.keysym.unicode;
+      }
+      else if((event.key.keysym.unicode >= (Uint16)'0') && (event.key.keysym.unicode <= (Uint16)'9'))
+      {
+        str += (char)event.key.keysym.unicode;
+      }
+      else if((event.key.keysym.unicode >= (Uint16)'A') && (event.key.keysym.unicode <= (Uint16)'Z'))
+      {
+        str += (char)event.key.keysym.unicode;
+      }
+      else if((event.key.keysym.unicode >= (Uint16)'a') && (event.key.keysym.unicode <= (Uint16)'z'))
+      {
+        str += (char)event.key.keysym.unicode;
+      }
+    }
+
+    if((event.key.keysym.sym == SDLK_BACKSPACE) && (str.length() != 0))
+    {
+      str.erase(str.length() - 1);
+    }
+
+    if(str != temp)
+    {
+      SDL_FreeSurface(text);
+      text = TTF_RenderText_Solid(font, str.c_str(), textColor);
+    }
   }
 }
