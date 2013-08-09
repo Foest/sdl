c0b29438ed4611fbeef2d80e81653064e28574f0
foest <foest88@gmail.com>
2013-08-08 20:22:16 -0700

    add Tile constructor

--- tiling.cpp
+++ tiling.cpp
@@ -493,3 +493,12 @@ bool Particle::is_dead()
 
   return false;
 }
+
+Tile::Tile(int x, int y, int tileType)
+{
+  box.x = x;
+  box.y = y;
+  box.w = TILE_WIDTH;
+  box.h = TILE_HEIGHT;
+  type = tileType;
+}
